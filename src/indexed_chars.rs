b449bc68a1312d9869eb677c533bf5cadf510b08
ultrabear <bearodark@gmail.com>
2023-08-27 14:40:45 -0700

    split out the inner type

---
+++ src/indexed_chars.rs
@@ -0,0 +1,114 @@
+use alloc::vec::Vec;
+
+#[derive(Debug)]
+pub(crate) struct IndexedCharsInner {
+    chars: Vec<u8>,
+    rollovers: Vec<usize>,
+}
+
+impl IndexedCharsInner {
+    pub(crate) fn new(s: &str) -> Self {
+        // this is expensive but it lets us avoid big reallocs
+        // it also lets us niche on ascii strings
+        let charlen = s.chars().count();
+
+        // if the number of chars is equal to the number of bytes we can skip allocating at all
+        // this lets us niche on an ascii string
+        if charlen == s.len() {
+            return Self {
+                chars: Vec::new(),
+                rollovers: Vec::new(),
+            };
+        }
+
+        let mut chars = Vec::with_capacity(charlen);
+        let mut rollovers = Vec::new();
+
+        for (char_idx, (real_idx, _)) in s.char_indices().enumerate() {
+            let mut offset_idx = real_idx - char_idx;
+
+            let u8_max = usize::from(u8::MAX);
+
+            offset_idx -= rollovers.len() * u8_max;
+
+            if offset_idx > u8_max {
+                rollovers.push(char_idx);
+                offset_idx -= u8_max;
+
+                debug_assert!(offset_idx <= u8_max);
+            }
+
+            // unwrap safe as chars cannot grow by more than 255 bytes at once
+            // and we just checked if it was over 255 bytes, conditionally subtracting
+            chars.push(offset_idx.try_into().unwrap());
+        }
+
+        // ensure we did not waste memory
+        debug_assert!(chars.capacity() == chars.len());
+
+        Self { chars, rollovers }
+    }
+
+    pub(crate) fn get_char(&self, buf: &str, index: usize) -> Option<char> {
+        // niche on empty chars with nonempty buf (ascii optimization)
+        if self.chars.is_empty() & !buf.is_empty() {
+            return buf.get(index..).and_then(|c| c.chars().next());
+        }
+
+        // if its in self.chars we can assume its in buf
+        let mut offset = usize::from(*self.chars.get(index)?);
+
+        offset += usize::from(u8::MAX)
+            * self
+                .rollovers
+                .binary_search(&index)
+                // we inc by 1 if variant is Ok as we want to do the rollover of the
+                // index where it "would" be regardless if its found, never its actual location
+                .map_or_else(|e| e, |t| t + 1);
+
+        buf[index + offset..].chars().next()
+    }
+}
+
+#[cfg(test)]
+extern crate std;
+
+#[test]
+fn create() {
+    use alloc::format;
+
+    let s_buf = "foo";
+    let s = IndexedCharsInner::new(s_buf);
+
+    assert!(s.rollovers.is_empty());
+
+    let special = '💯';
+
+    let foo_alloc = format!("{special}a");
+
+    let foo = IndexedCharsInner::new(&foo_alloc);
+
+    assert_eq!(foo.chars, &[0, (special.len_utf8() - 1) as u8]);
+}
+
+#[test]
+fn get_idx() {
+    use alloc::string::String;
+    use rand::{seq::SliceRandom, thread_rng};
+
+    let mut chars: Vec<_> = (0..20_000)
+        .map(|i| char::from_u32(i as u32).unwrap())
+        .cycle()
+        .take(1_000_000)
+        .collect();
+
+    chars.shuffle(&mut thread_rng());
+
+    let s = String::from_iter(&chars);
+
+    let index = IndexedCharsInner::new(&s);
+
+    for (char_idx, (_real_idx, c)) in s.char_indices().enumerate() {
+        assert_eq!(index.get_char(&s, char_idx).unwrap(), c);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! # `char_index`
 //! A crate that provides a tradeoff of space efficiency and apparent O(1) charwise indexing.  
 //!
-//! To get started, create a new [`IndexedChars`] instance.
+//! To get started, create a new [`IndexedChars`] or [`OwnedIndexedChars`] instance.
 //!
 //! # How it Works
 //! [`IndexedChars`] works by allocating a `Vec<u8>` under the hood that stores char
@@ -15,17 +15,20 @@
 //! This has a tradeoff where after 255 bytes worth of string data that is not a single byte or,
 //! the sum of all the characters that exceed one bytes lengths minus the first byte, has been added
 //! to the string, it can no longer store data, as it would overflow u8.
-//! This is where the rollovers structure takes effect, it is a `Vec<u32>` whose sole purpose is to
+//! This is where the rollovers structure takes effect, it is a `Vec<usize>` whose sole purpose is to
 //! store indexes where a rollover has occurred, this can then be trivially binary searched to find the
 //! current index we are working with, and then apply the length of the list at that point multiplied by u8::MAX to find the true offset of the character.
 //!
 //! In this way, we achieve what behaves as an O(1) char lookup (technically worst case O(log n)) for most strings, while saving memory over `Vec<char>` (sans the case where the string is only made up of 4 byte characters, which acts as the worst case for time complexity too).
 //!
-
-#![allow(dead_code)] // TODO remove
+//! Additionally, as a niche optimization, if the string contains only ascii (all offsets 0); it will simply not allocate any extra memory, and gain perfect O(1) lookup.
+//!
 
 extern crate alloc;
-use alloc::vec::Vec;
+use alloc::string::String;
+
+mod indexed_chars;
+use indexed_chars::IndexedCharsInner;
 
 /// A string whose char indices have been cached for ~O(1) char lookup.  
 ///
@@ -36,101 +39,46 @@ use alloc::vec::Vec;
 /// As the number of non ascii characters increases, the data density will worsen, until the potential worst case of 5 bytes per character.
 ///
 /// The internal representation of this type allows for up to 255 bytes of non ascii unicode chars before an internal rollover occurs (thus tending the complexity towards O(log n)), this is the tradeoff made to reduce memory usage. See the section [`How it Works`] for details on why char indexing worst case is O(log n), and why in practical cases it appears to be O(1).
-///
-/// No string passed to this type may be more than 4 gigabytes.
 #[derive(Debug)]
 pub struct IndexedChars<'a> {
     buf: &'a str,
-    chars: Vec<u8>,
-    rollovers: Vec<usize>,
+    inner: IndexedCharsInner,
 }
 
 impl<'a> IndexedChars<'a> {
     pub fn new(s: &'a str) -> Self {
-        let mut chars = Vec::new();
-        let mut rollovers = Vec::new();
+        let inner = IndexedCharsInner::new(s);
 
-        for (char_idx, (real_idx, _)) in s.char_indices().enumerate() {
-            let mut offset_idx = real_idx - char_idx;
-
-            let u8_max = usize::from(u8::MAX);
-
-            offset_idx -= rollovers.len() * u8_max;
-
-            if offset_idx > u8_max {
-                rollovers.push(char_idx);
-                offset_idx -= u8_max;
-
-                debug_assert!(offset_idx <= u8_max);
-            }
-
-            // unwrap safe as chars cannot grow by more than 255 bytes at once
-            // and we just checked if it was over 255 bytes, conditionally subtracting
-            chars.push(offset_idx.try_into().unwrap());
-        }
-
-        Self {
-            buf: s,
-            chars,
-            rollovers,
-        }
+        Self { buf: s, inner }
     }
 
-    pub fn get(&self, index: usize) -> Option<char> {
-        // if its in self.chars we can assume its in buf
-        let mut offset = usize::from(*self.chars.get(index)?);
-
-        offset += usize::from(u8::MAX)
-            * self
-                .rollovers
-                .binary_search(&index)
-                // we inc by 1 if variant is Ok as we want to do the rollover of the
-                // index where it "would" be regardless if its found, never its actual location
-                .map_or_else(|e| e, |t| t + 1);
-
-        self.buf[index + offset..].chars().next()
+    pub fn get_char(&self, index: usize) -> Option<char> {
+        self.inner.get_char(self.buf, index)
     }
 }
 
-#[cfg(test)]
-extern crate std;
-
-#[test]
-fn create() {
-    use alloc::format;
-
-    let s = IndexedChars::new("foo");
-
-    assert_eq!(s.chars, &[0, 0, 0]);
-    assert!(s.rollovers.is_empty());
-
-    let special = '💯';
-
-    let foo_alloc = format!("{special}a");
-
-    let foo = IndexedChars::new(&foo_alloc);
-
-    assert_eq!(foo.chars, &[0, (special.len_utf8() - 1) as u8]);
+/// A string whose char indices have been cached for ~O(1) char lookup. Owned variant.
+///
+/// This structure allocates 1 additional bytes per unicode scalar value,
+/// which in the case of ascii will only use 2 total bytes for a
+/// single char (as opposed to the 4 bytes required in `Vec<char>`).
+///
+/// As the number of non ascii characters increases, the data density will worsen, until the potential worst case of 5 bytes per character.
+///
+/// The internal representation of this type allows for up to 255 bytes of non ascii unicode chars before an internal rollover occurs (thus tending the complexity towards O(log n)), this is the tradeoff made to reduce memory usage. See the section [`How it Works`] for details on why char indexing worst case is O(log n), and why in practical cases it appears to be O(1).
+pub struct OwnedIndexedChars {
+    buf: String,
+    inner: IndexedCharsInner,
 }
 
-#[test]
-fn get_idx() {
-    use alloc::string::String;
-    use rand::{seq::SliceRandom, thread_rng};
+impl OwnedIndexedChars {
+    pub fn new(s: String) -> Self {
+        let inner = IndexedCharsInner::new(&s);
 
-    let mut chars: Vec<_> = (0..10_000)
-        .map(|i| char::from_u32(i as u32).unwrap())
-        .cycle()
-        .take(100_000)
-        .collect();
+        Self { buf: s, inner }
+    }
 
-    chars.shuffle(&mut thread_rng());
-
-    let s = String::from_iter(&chars);
-
-    let index = IndexedChars::new(&s);
-
-    for (char_idx, (_real_idx, c)) in s.char_indices().enumerate() {
-        assert_eq!(index.get(char_idx).unwrap(), c);
+    pub fn get_char(&self, index: usize) -> Option<char> {
+        self.inner.get_char(&self.buf, index)
     }
 }
